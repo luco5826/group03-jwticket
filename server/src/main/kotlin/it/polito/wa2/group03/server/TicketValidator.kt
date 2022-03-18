bbb176b5dc6b5217e0180abf2eda24267f4662e2
Ale142 <ale.bacci142@gmail.com>
2022-03-18 18:30:35 +0100

    handled jwt expiration date and validity zone

--- server/src/main/kotlin/it/polito/wa2/group03/server/TicketValidator.kt
+++ server/src/main/kotlin/it/polito/wa2/group03/server/TicketValidator.kt
@@ -7,6 +7,9 @@ import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
+import java.time.LocalDateTime
+import java.time.ZoneOffset
+import java.util.Locale
 
 
 @RestController
@@ -17,8 +20,18 @@ class TicketValidator {
     @PostMapping("/validate")
     fun validateTicker(@RequestBody payload: TicketPayload): ResponseEntity<String> {
         try {
-            var decodified = parser.parse(payload.token)
+            var decodified = parser.parseClaimsJws(payload.token)
+            if(!decodified.body.getValue("vz").toString().contains(payload.zone)) {
+                return ResponseEntity.status(HttpStatus.FORBIDDEN).body("Zone not supported")
+            }
+
+            println(LocalDateTime.ofEpochSecond(decodified.body.getValue("exp").toString().toLong(), 0, ZoneOffset.ofHours(1)))
+            if(LocalDateTime.ofEpochSecond(decodified.body.getValue("exp").toString().toLong(), 0, ZoneOffset.ofHours(1)) < LocalDateTime.now()) {
+                return ResponseEntity.status(HttpStatus.FORBIDDEN).body("Ticket expired")
+            }
+
         } catch (e: java.lang.Exception) {
+            println(e.message)
             return ResponseEntity.status(HttpStatus.FORBIDDEN).body("Not valid")
         }
         return ResponseEntity.status(HttpStatus.OK).body("Valid")
