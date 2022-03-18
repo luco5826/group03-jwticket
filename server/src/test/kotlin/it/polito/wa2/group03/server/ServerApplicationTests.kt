37c6be1ac0bdd3dd75b3a068a71f4c5ccae3f668
Ale142 <ale.bacci142@gmail.com>
2022-03-18 16:36:39 +0100

    initial spring commit

---
+++ server/src/test/kotlin/it/polito/wa2/group03/server/ServerApplicationTests.kt
@@ -0,0 +1,13 @@
+package it.polito.wa2.group03.server
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ServerApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
