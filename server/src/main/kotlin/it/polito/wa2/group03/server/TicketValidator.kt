962afb5144e6195de7133aacb954b078434f683d
Luca Errani <luca.errani1@gmail.com>
2022-03-18 18:51:50 +0100

    Fix expiration date parsing

--- server/src/main/kotlin/it/polito/wa2/group03/server/TicketValidator.kt
+++ server/src/main/kotlin/it/polito/wa2/group03/server/TicketValidator.kt
@@ -1,5 +1,7 @@
 package it.polito.wa2.group03.server
 
+import io.jsonwebtoken.ExpiredJwtException
+import io.jsonwebtoken.JwtParser
 import io.jsonwebtoken.Jwts
 import org.apache.tomcat.util.codec.binary.Base64
 import org.springframework.http.HttpStatus
@@ -7,31 +9,26 @@ import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
-import java.time.LocalDateTime
-import java.time.ZoneOffset
-import java.util.Locale
+
+
 
 
 @RestController
 class TicketValidator {
     val key = "ebFWkwyCkiYWbmZhoDvOOKSQnRayUzpOfQpfBWLWeshroGkQFULEkxwdRMvbjKYb"
-    val parser = Jwts.parserBuilder().setSigningKey(Base64.encodeBase64String(key.toByteArray())).build()
+    val parser: JwtParser = Jwts.parserBuilder().setSigningKey(Base64.encodeBase64String(key.toByteArray())).build()
 
     @PostMapping("/validate")
     fun validateTicker(@RequestBody payload: TicketPayload): ResponseEntity<String> {
         try {
-            var decodified = parser.parseClaimsJws(payload.token)
-            if(!decodified.body.getValue("vz").toString().contains(payload.zone)) {
+            val decodified = parser.parseClaimsJws(payload.token)
+            if (!decodified.body.getValue("vz").toString().contains(payload.zone)) {
                 return ResponseEntity.status(HttpStatus.FORBIDDEN).body("Zone not supported")
             }
 
-            println(LocalDateTime.ofEpochSecond(decodified.body.getValue("exp").toString().toLong(), 0, ZoneOffset.ofHours(1)))
-            if(LocalDateTime.ofEpochSecond(decodified.body.getValue("exp").toString().toLong(), 0, ZoneOffset.ofHours(1)) < LocalDateTime.now()) {
-                return ResponseEntity.status(HttpStatus.FORBIDDEN).body("Ticket expired")
-            }
-
-        } catch (e: java.lang.Exception) {
-            println(e.message)
+        }catch (e: ExpiredJwtException) {
+            return ResponseEntity.status(HttpStatus.FORBIDDEN).body("Ticket expired")
+        } catch (e: Exception) {
             return ResponseEntity.status(HttpStatus.FORBIDDEN).body("Not valid")
         }
         return ResponseEntity.status(HttpStatus.OK).body("Valid")
