f8d2c3d694327ea0171d6794cf7900d1e526f7fc
riccardomengoli <mengoli.riccardo@gmail.com>
2022-03-29 00:46:06 +0200

    Created plot for stateful server
    Randomized token
    Updated benchmark values

--- server/src/main/kotlin/it/polito/wa2/group03/server/benchmark/ExpectedBenchmark.kt
+++ server/src/main/kotlin/it/polito/wa2/group03/server/benchmark/ExpectedBenchmark.kt
@@ -12,31 +12,39 @@ import jetbrains.letsPlot.export.ggsave
  * measurements: Accept a list of pair<NumberOfClients,Throughput as rps> of at least 6 elements
  */
 class ExpectedBenchmark(private val measurements: List<Pair<Int, Double>>) {
-    fun buildModel(): (Int) -> Pair<Int, Double> {
-        val model = Model.build(measurements.asIterable()
+    private val maxNumberOfClients = 256
+    private val model: (Int) -> Pair<Int, Double>
+    init {
+        val modelList = Model.build(measurements.asIterable()
             .map { Measurement.ofConcurrency().andThroughput(it.first.toDouble(), it.second) }
             .toMutableList())
 
-        return {i: Int -> Pair(i, model.throughputAtConcurrency(i.toDouble()))}
+        model = {i: Int -> Pair(i, modelList.throughputAtConcurrency(i.toDouble()))}
     }
 
-    fun plot() {
+    fun createCSV(filename: String) {
+        val file = File("server/src/main/kotlin/it/polito/wa2/group03/server/benchmark/$filename.csv")
+        file.writeText("clients,rps\n")
+        for (i in 1..maxNumberOfClients) {
+            file.appendText("${model(i).first},${model(i).second}\n")
+        }
+    }
+
+    fun plot(filename: String) {
+        // Real measurements
         val (xs,ys) = measurements.unzip()
         val realData = mapOf (
                 "Concurrency" to xs,
                 "Throughput" to ys
         )
 
-        val model = buildModel()
-        var i = 1
-        val maxNumberOfClients = 256
+        // USL theoretical values
         val xs2 = mutableListOf<Int>()
         val ys2 = mutableListOf<Double>()
-        while (i <= maxNumberOfClients) {
+        for (i in 1..maxNumberOfClients) {
             val thrAtI = model(i)
             xs2.add(thrAtI.first)
             ys2.add(thrAtI.second)
-            i++
         }
         val modelData = mapOf (
                 "Concurrency" to xs2,
@@ -50,23 +58,20 @@ class ExpectedBenchmark(private val measurements: List<Pair<Int, Double>>) {
                 scaleXDiscrete(name="Concurrency", breaks=xs, labels=xs.map{it.toString()})
 
 
-        ggsave(p, "concurrencyPlot.svg", path="server/src/main/kotlin/it/polito/wa2/group03/server/benchmark")
+        ggsave(p, "$filename.svg", path="server/src/main/kotlin/it/polito/wa2/group03/server/benchmark")
     }
 }
 
 fun main() {
-    val fileName = File("server/src/main/kotlin/it/polito/wa2/group03/server/benchmark/results.csv")
-    val measurements = listOf((1 to 570.0), (2 to 1313.0), (4 to 1686.0), (8 to 2204.0), (16 to 2340.0),
-            (32 to 2469.0), (64 to 2592.0), (128 to 2564.0), (256 to 2517.0))
-    val benchmark = ExpectedBenchmark(measurements)
-    val benchmarkModel = benchmark.buildModel()
-    val maxNumberOfClients = 256
-    var i = 1
-    fileName.writeText("clients,rps\n")
-    while (i <= maxNumberOfClients) {
-        fileName.appendText("${benchmarkModel(i).first},${benchmarkModel(i).second}\n")
-        i++
-    }
+    val statelessMeasurements = listOf((1 to 540.0), (2 to 1191.0), (4 to 1521.0), (8 to 1968.0), (16 to 2004.0),
+            (32 to 2204.0), (64 to 2257.0), (128 to 2283.0), (256 to 2229.0))
+    val statelessBenchmark = ExpectedBenchmark(statelessMeasurements)
+    statelessBenchmark.createCSV("statelessResults")
+    statelessBenchmark.plot("statelessConcurrencyPlot")
 
-    benchmark.plot()
+    val statefulMeasurements = listOf((1 to 450.0), (2 to 857.0), (4 to 1129.0), (8 to 1634.0), (16 to 1745.0),
+            (32 to 1711.0), (64 to 1643.0), (128 to 1553.0), (256 to 1376.0))
+    val statefulBenchmark = ExpectedBenchmark(statefulMeasurements)
+    statefulBenchmark.createCSV("statefulResults")
+    statefulBenchmark.plot("statefulConcurrencyPlot")
 }
