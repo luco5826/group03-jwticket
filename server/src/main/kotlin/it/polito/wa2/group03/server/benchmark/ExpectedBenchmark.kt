c557763c63f1e89930dd00313f9443d21c497c7b
riccardomengoli <mengoli.riccardo@gmail.com>
2022-03-31 13:00:17 +0200

    Added plots for keepalive and timeout cases

--- server/src/main/kotlin/it/polito/wa2/group03/server/benchmark/ExpectedBenchmark.kt
+++ server/src/main/kotlin/it/polito/wa2/group03/server/benchmark/ExpectedBenchmark.kt
@@ -1,4 +1,5 @@
 package it.polito.wa2.group03.server.benchmark
+
 import com.codahale.usl4j.Measurement
 import com.codahale.usl4j.Model
 import java.io.File
@@ -8,18 +9,26 @@ import jetbrains.letsPlot.geom.geomPoint
 import jetbrains.letsPlot.scale.scaleXDiscrete
 import jetbrains.letsPlot.export.ggsave
 
+data class MeasurementL(
+        val concurrentClients: Int,
+        val rps: Double,
+        val totalRequests: Int,
+        val totalErrors: Int
+)
+
 /**
- * measurements: Accept a list of pair<NumberOfClients,Throughput as rps> of at least 6 elements
+ * measurements: Accept a list of measurements of at least 6 elements
  */
-class ExpectedBenchmark(private val measurements: List<Pair<Int, Double>>) {
+class ExpectedBenchmark(private val measurements: List<MeasurementL>) {
     private val maxNumberOfClients = 256
     private val model: (Int) -> Pair<Int, Double>
+
     init {
         val modelList = Model.build(measurements.asIterable()
-            .map { Measurement.ofConcurrency().andThroughput(it.first.toDouble(), it.second) }
-            .toMutableList())
+                .map { Measurement.ofConcurrency().andThroughput(it.concurrentClients.toDouble(), it.rps) }
+                .toMutableList())
 
-        model = {i: Int -> Pair(i, modelList.throughputAtConcurrency(i.toDouble()))}
+        model = { i: Int -> Pair(i, modelList.throughputAtConcurrency(i.toDouble())) }
     }
 
     fun createCSV(filename: String) {
@@ -30,12 +39,20 @@ class ExpectedBenchmark(private val measurements: List<Pair<Int, Double>>) {
         }
     }
 
-    fun plot(filename: String) {
+    fun plot(filename: String, plotErrors: Boolean = false) {
         // Real measurements
-        val (xs,ys) = measurements.unzip()
-        val realData = mapOf (
+        val xs = mutableListOf<Int>()
+        val ys = mutableListOf<Double>()
+        val ysErr = mutableListOf<Int>()
+        measurements.forEach {
+            xs.add(it.concurrentClients)
+            ys.add(it.rps)
+            ysErr.add(it.totalErrors)
+        }
+        val realData = mapOf(
                 "Concurrency" to xs,
-                "Throughput" to ys
+                "Throughput" to ys,
+                "Errors" to ysErr
         )
 
         // USL theoretical values
@@ -46,32 +63,86 @@ class ExpectedBenchmark(private val measurements: List<Pair<Int, Double>>) {
             xs2.add(thrAtI.first)
             ys2.add(thrAtI.second)
         }
-        val modelData = mapOf (
+        val modelData = mapOf(
                 "Concurrency" to xs2,
                 "Throughput" to ys2
         )
 
         val p = letsPlot(null) { x = "Concurrency"; y = "Throughput" } +
-                geomLine(modelData, size=0.7, color="red") +
-                geomLine(realData, size=1) +
-                geomPoint(realData, size=3) +
-                scaleXDiscrete(name="Concurrency", breaks=xs, labels=xs.map{it.toString()})
+                geomLine(modelData, size = 0.7, color = "red") +
+                geomLine(realData, size = 1) +
+                geomPoint(realData, size = 3) +
+                scaleXDiscrete(name = "Concurrency", breaks = xs, labels = xs.map { it.toString() })
 
+        ggsave(p, "$filename.svg", path = "server/src/main/kotlin/it/polito/wa2/group03/server/benchmark")
 
-        ggsave(p, "$filename.svg", path="server/src/main/kotlin/it/polito/wa2/group03/server/benchmark")
+        if (plotErrors) {
+            val pErr = letsPlot(null) { x = "Concurrency"; y = "Errors" } +
+                    geomLine(realData, size = 1) +
+                    geomPoint(realData, size = 3) +
+                    scaleXDiscrete(name = "Concurrency", breaks = xs, labels = xs.map { it.toString() })
+            ggsave(pErr, "${filename}Errors.svg", path = "server/src/main/kotlin/it/polito/wa2/group03/server/benchmark")
+        }
     }
 }
 
 fun main() {
-    val statelessMeasurements = listOf((1 to 540.0), (2 to 1191.0), (4 to 1521.0), (8 to 1968.0), (16 to 2004.0),
-            (32 to 2204.0), (64 to 2257.0), (128 to 2283.0), (256 to 2229.0))
-    val statelessBenchmark = ExpectedBenchmark(statelessMeasurements)
-    statelessBenchmark.createCSV("statelessResults")
-    statelessBenchmark.plot("statelessConcurrencyPlot")
+    var benchmark: ExpectedBenchmark
+    val statelessMeasurements = listOf(
+            MeasurementL(1, 540.0, 10000, 0),
+            MeasurementL(2, 1191.0, 10000, 0),
+            MeasurementL(4, 1521.0, 10000, 0),
+            MeasurementL(8, 1968.0, 10000, 0),
+            MeasurementL(16, 2004.0, 10000, 0),
+            MeasurementL(32, 2204.0, 10000, 0),
+            MeasurementL(64, 2257.0, 10000, 0),
+            MeasurementL(128, 2283.0, 10000, 0),
+            MeasurementL(256, 2229.0, 10000, 0)
+    )
+    benchmark = ExpectedBenchmark(statelessMeasurements)
+    benchmark.createCSV("statelessResults")
+    benchmark.plot("statelessConcurrency")
 
-    val statefulMeasurements = listOf((1 to 450.0), (2 to 857.0), (4 to 1129.0), (8 to 1634.0), (16 to 1745.0),
-            (32 to 1711.0), (64 to 1643.0), (128 to 1553.0), (256 to 1376.0))
-    val statefulBenchmark = ExpectedBenchmark(statefulMeasurements)
-    statefulBenchmark.createCSV("statefulResults")
-    statefulBenchmark.plot("statefulConcurrencyPlot")
+    val statefulMeasurements = listOf(
+            MeasurementL(1, 450.0, 10000, 8),
+            MeasurementL(2, 857.0, 10000, 12),
+            MeasurementL(4, 1129.0, 10000, 19),
+            MeasurementL(8, 1634.0, 10000, 34),
+            MeasurementL(16, 1745.0, 10000, 41),
+            MeasurementL(32, 1711.0, 10000, 53),
+            MeasurementL(64, 1643.0, 10000, 56),
+            MeasurementL(128, 1553.0, 10000, 61),
+            MeasurementL(256, 1376.0, 10000, 77)
+    )
+    benchmark = ExpectedBenchmark(statefulMeasurements)
+    benchmark.createCSV("statefulResults")
+    benchmark.plot("statefulConcurrency")
+
+    val statefulKeepalive = listOf(
+            MeasurementL(1, 731.0, 10000, 5),
+            MeasurementL(2, 1674.0, 10000, 16),
+            MeasurementL(4, 2473.0, 10000, 17),
+            MeasurementL(8, 3172.0, 10000, 33),
+            MeasurementL(16, 2969.0, 10000, 45),
+            MeasurementL(32, 2804.0, 10000, 47),
+            MeasurementL(64, 2578.0, 10000, 47),
+            MeasurementL(128, 2221.0, 10000, 66),
+            MeasurementL(256, 1878.0, 10000, 79)
+    )
+    benchmark = ExpectedBenchmark(statefulKeepalive)
+    benchmark.plot("statefulConcurrencyKeepalive")
+
+    val statefulTimeout100 = listOf(
+            MeasurementL(1, 468.0, 10000, 7),
+            MeasurementL(2, 872.0, 10000, 15),
+            MeasurementL(4, 1178.0, 10000, 19),
+            MeasurementL(8, 1483.0, 10000, 38),
+            MeasurementL(16, 1516.0, 10000, 31),
+            MeasurementL(32, 1580.0, 10000, 48),
+            MeasurementL(64, 1529.0, 10000, 56),
+            MeasurementL(128, 1489.0, 10000, 186),
+            MeasurementL(256, 1323.0, 10000, 2059)
+    )
+    benchmark = ExpectedBenchmark(statefulTimeout100)
+    benchmark.plot("statefulConcurrencyTimeout", true)
 }
